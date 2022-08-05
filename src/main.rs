cf1ad021c6951363dd94e71b1f05c9f47b6010fa
daunJung <daun.timothy@gmail.com>
2022-08-06 00:22:05 +0900

    Update: Hello world!
    - first lesson.
    - just import ferris-says and it show quty crab on terminal. lol

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io::{stdout, BufWriter};
+
+use ferris_says::say;
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let message = String::from("Hello fellow Rustaceans");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
 }
